fb4827ea947121961398d21a39857916fb8b94e1
wesley kaihara <wesleykaihara@gmail.com>
2024-05-05 02:31:30 -0300

    Service Cards

--- src/app/destaque/page.tsx
+++ src/app/destaque/page.tsx
@@ -1,11 +1,53 @@
 "use client";
 
-import Header from "@/components/Header";
+import ImageContent from "@/components/ImageContent";
+import ServiceCard from "@/components/ServiceCard";
+import TitleWithSubtitle from "@/components/Title/TitleWithSubtitle";
 
 export default async function Destaque() {
   return (
-    <>
-      <h1 className="text-5xl">Destaque</h1>
-    </>
+    <main className="px-4">
+      <section className="flex justify-center">
+        <div className="container mx-20">
+          <ImageContent imageUrl="startup_banner.jpg">
+            <TitleWithSubtitle
+              subtitle="NOSSOS SERVIÇOS"
+              title="Atividades e Conhecimentos"
+            />
+          </ImageContent>
+        </div>
+      </section>
+      <section className="mt-20">
+        <div className="flex flex-col justify-center items-center">
+          <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-4xl mb-4">
+            Serviços
+          </h1>
+          <h2 className="text-5xl sm:text-6xl md:text-7xl lg:text-6xl font-bold mb-8">
+            em Destaque
+          </h2>
+          <p className="text-center max-w-lg text-lg mb-8">
+            Crie seu próprio caminho na tecnologia com o StartHub. Suas ideias
+            podem se tornar realidade: desenvolva soluções para transformar o
+            mundo.
+          </p>
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 w-full max-w-screen-lg">
+            <ServiceCard
+              title="Sugestões de Ferramentas"
+              description="Receba recomendações especializadas para impulsionar o desenvolvimento, marketing, finanças e muito mais. Economize tempo e recursos, concentrando-se nas soluções certas para o crescimento do seu negócio."
+              features={[
+                "Inteligência Artificial",
+                "Economia de Tempo",
+                "Maior Assertividade",
+              ]}
+            />
+            <ServiceCard
+              title="Geração de Nomes"
+              description="Receba recomendações especializadas para impulsionar o desenvolvimento, marketing, finanças e muito mais. Economize tempo e recursos, concentrando-se nas soluções certas para o crescimento do seu negócio."
+              features={["Maior relevância", "Criatividade"]}
+            />
+          </div>
+        </div>
+      </section>
+    </main>
   );
 }
--- src/app/startups/cadastro/page.tsx
+++ src/app/startups/cadastro/page.tsx
@@ -1,9 +1,8 @@
 "use client";
-
 import Banner from "@/components/Banner";
 import Title from "@/components/Title";
 import { ProjectService } from "@/services/ProjectService";
-import React, { ChangeEvent, FormEvent, useState } from "react";
+import React, { ChangeEvent, FormEvent, useRef, useState } from "react";
 
 interface StartupData {
   name: string;
@@ -21,6 +20,8 @@ const StartupForm: React.FC = () => {
     previewImage: null,
     private: false,
   });
+
+  const fileInputRef = useRef<HTMLInputElement>(null);
 
   const handleInputChange = (
     e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
@@ -44,6 +45,12 @@ const StartupForm: React.FC = () => {
         });
       };
       reader.readAsDataURL(file);
+    }
+  };
+
+  const handleClickUpload = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
     }
   };
 
@@ -89,77 +96,83 @@ const StartupForm: React.FC = () => {
         <Title>Cadastrar uma Startup</Title>
         <form
           onSubmit={handleSubmit}
-          className="max-w mx-auto px-6 py-10 bg-gray-100 shadow-lg rounded-lg"
+          className="max-w mx-auto px-6 py-10 bg-gray-100 shadow-lg rounded-lg md:grid md:grid-cols-2 md:gap-4"
         >
-          <div className="mb-6">
-            <label
-              htmlFor="name"
-              className="text-3xl block text-dark font-bold mb-2"
-            >
-              Nome
-            </label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              value={startupData.name}
-              placeholder="Informe o nome do seu produto (Pode ser alterado posteriormente)"
-              onChange={handleInputChange}
-              className="w-full px-3 py-3 border rounded-lg text-dark focus:outline-none focus:border-blue-500"
-              required
-            />
-          </div>
-          <div className="mb-6">
-            <label
-              htmlFor="description"
-              className="text-3xl block text-dark font-bold mb-2"
-            >
-              Descrição:
-            </label>
-            <textarea
-              id="description"
-              name="description"
-              value={startupData.description}
-              onChange={handleInputChange}
-              placeholder="Escreva uma descrição sobre seu produto, quais problemas ele busca resolver, pesquisas realizadas no mercado, empresas parceiras e outros tópicos importantes para chamar a atenção de possíveis clientes e colaboradores"
-              className="w-full px-3 py-3 border rounded-lg text-gray-700 focus:outline-none focus:border-blue-500"
-              rows={4}
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label
-              htmlFor="image"
-              className="text-3xl block text-gray-700 font-bold mb-2"
-            >
-              Imagem:
-            </label>
-            <input
-              type="file"
-              id="image"
-              name="image"
-              accept="image/*"
-              onChange={handleImageChange}
-              className="w-full"
-              required
-            />
-            <span className="text-gray-500 text-sm">
-              Selecione uma imagem para a sua startup
-            </span>
-          </div>
-
-          {startupData.previewImage && (
-            <div className="mb-4">
-              <img
-                src={startupData.previewImage}
-                alt="Imagem da Startup"
-                className="w-full h-auto"
+          <div className="mb-6 md:mb-0">
+            <div className="mb-6">
+              <label
+                htmlFor="name"
+                className="text-3xl block text-dark font-bold mb-2"
+              >
+                Nome
+              </label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                value={startupData.name}
+                placeholder="Informe o nome do seu produto (Pode ser alterado posteriormente)"
+                onChange={handleInputChange}
+                className="w-full px-3 py-3 border rounded-lg text-dark focus:outline-none focus:border-blue-500"
+                required
               />
             </div>
-          )}
+            <div className="mb-6">
+              <label
+                htmlFor="description"
+                className="text-3xl block text-dark font-bold mb-2"
+              >
+                Descrição:
+              </label>
+              <textarea
+                id="description"
+                name="description"
+                value={startupData.description}
+                onChange={handleInputChange}
+                placeholder="Escreva uma descrição sobre seu produto, quais problemas ele busca resolver, pesquisas realizadas no mercado, empresas parceiras e outros tópicos importantes para chamar a atenção de possíveis clientes e colaboradores"
+                className="w-full px-3 py-3 border rounded-lg text-gray-700 focus:outline-none focus:border-blue-500"
+                rows={15}
+                required
+              />
+            </div>
+          </div>
+          <div className="mb-4 w-full flex justify-center items-center relative">
+            <label htmlFor="image" className="cursor-pointer">
+              <div className="relative w-96 h-96">
+                {startupData.previewImage ? (
+                  <img
+                    src={startupData.previewImage}
+                    alt="Imagem da Startup"
+                    className="w-full h-full object-cover"
+                    onClick={handleClickUpload}
+                  />
+                ) : (
+                  <img
+                    src="/logo.png"
+                    alt="Upload Icon"
+                    className="w-full h-full cursor-pointer object-cover"
+                    onClick={handleClickUpload}
+                  />
+                )}
+                <span className="absolute top-2 left-2 bg-white text-xs px-2 py-1 rounded-md">
+                  Tamanho recomendado: 300x300
+                </span>
+              </div>
+              <input
+                type="file"
+                id="image"
+                name="image"
+                accept="image/*"
+                onChange={handleImageChange}
+                className="hidden"
+                ref={fileInputRef}
+                required
+              />
+            </label>
+          </div>
           <button
             type="submit"
-            className="w-64 mt-3 bg-primary transition duration-300 ease-in-out hover:filter hover:brightness-110 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            className="w-full md:w-64 mt-3 bg-primary transition duration-300 ease-in-out hover:filter hover:brightness-110 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
           >
             Salvar
           </button>
